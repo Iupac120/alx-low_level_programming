91d7c410930cade2fc4944194c89c6067b97d676
Iupac120 <oforokpara@gmail.com>
2023-03-20 08:31:44 -0700

    Printf added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A programme that uses the put function to write a script
  * return: 0 (success)
  */
- int main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
