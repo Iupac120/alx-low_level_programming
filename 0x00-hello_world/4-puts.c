a05573edc1a2afa4129b45536e2c1c6a09425724
Iupac120 <oforokpara@gmail.com>
2023-03-20 08:34:55 -0700

    Puts updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main - A programme that uses the put function to write a script
- * return: 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
- 
