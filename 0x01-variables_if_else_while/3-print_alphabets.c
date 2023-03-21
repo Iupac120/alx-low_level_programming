6a66c36b8ded5c65c96fe6362fc1b6748ddc2773
Iupac120 <oforokpara@gmail.com>
2023-03-21 06:23:37 -0700

    Print alpbabets in upper and lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,9 +3,11 @@
  * main - A program that prints the alphabet in lowercase, and then in uppercase
  * Return: 0(Success)
  */
+
 int main(void)
 {
 	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
