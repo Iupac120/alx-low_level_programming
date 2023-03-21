d1ca71b2c0670ca75db8e53c4c9b361d4cb5b193
Iupac120 <oforokpara@gmail.com>
2023-03-21 07:11:41 -0700

    Print number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program a alphabets in digit
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
