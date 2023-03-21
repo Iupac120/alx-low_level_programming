23327fda7eacec42dc8359b31c48db162173740f
Iupac120 <oforokpara@gmail.com>
2023-03-21 06:43:23 -0700

    Print alphabet added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase except q and e
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
