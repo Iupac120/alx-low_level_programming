b443f1e125b7683ef2b58918082454e504f9b76f
Iupac120 <oforokpara@gmail.com>
2023-03-21 15:37:47 -0700

    Combination 4 added

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,22 +12,25 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 1 ; j < 10 ; j++)
-		{
-			for (k = 2 ; k < 10 ; k++)
-			{
-				if (i < j && j < k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
+	for (j = 1 ; j < 10 ; j++)
+	{
+	for (k = 2 ; k < 10 ; k++)
+	{
+	if (i < j && j < k)
+	{
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+	if (i == 7 && j == 8)
+	continue;
+	{
+	putchar(',');
+	putchar(' ');
 	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }
