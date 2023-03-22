695312724b4fee80f61eabc51c6c288666b04bf9
Iupac120 <oforokpara@gmail.com>
2023-03-22 10:37:17 -0700

    added numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - A function name
+ *
+ * @a: function parameter
+ * @b: a function parameter
+ *
+ * Return: 0(Success)
+ */
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
