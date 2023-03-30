34e145f1b9410d3de17936eb47e7534728cde290
Iupac120 <oforokpara@gmail.com>
2023-03-30 15:22:45 -0700

    Encodes strin

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - function that encodes
+ * @str: function parameter
+ * Return: 09Success)
+ */
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		for (j = 0 ; a[j] != '\0' ; j++)
+		{
+			if (str[i] == a[j])
+				str[i] = b[j];
+		}
+	}
+	return (str);
+}
