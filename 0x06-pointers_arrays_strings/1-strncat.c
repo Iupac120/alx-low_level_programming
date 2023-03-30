f6ef55e55fab8e1d1da344ca84d1f4524c7ee8f6
Iupac120 <oforokpara@gmail.com>
2023-03-30 05:22:35 -0700

    String concatenated to n numbers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function that concatenate
+ * @dest: function parameter
+ * @src: function parameter
+ * @n: function parameter
+ * Return: 0(Success)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	for (i = 0 ; i <= n ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
